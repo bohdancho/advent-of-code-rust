33967e6b686e08d5582461d8c1bbe86020d2a523
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-14 16:23:07 +0200

    solve day 1 part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,36 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut sum = 0;
+    for line in input.lines() {
+        let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+        let first = digits.next().unwrap();
+        let last = digits.last().unwrap_or(first);
+        sum += first * 10 + last;
+    }
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let _ = input;
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
+        assert_eq!(result, Some(142));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
